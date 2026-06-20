0bd81467a2d83a40fe9bc2881424ba772f7a89af
Jackpot-make <waqieahmed7@gmail.com>
2026-06-20 18:33:43 +0500

    auto setup on deploy

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,5 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py migrate_schemas --shared
 python manage.py migrate_schemas || true
+python manage.py shell < setup_prod.py || true
 gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2 --timeout 120
